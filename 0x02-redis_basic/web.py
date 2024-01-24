41e16d8a7755a34dab443610a739ec2cf25ef085
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-25 01:36:25 +0300

    Refactor get_page function to use Redis for caching and counting requests

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,24 +9,12 @@ from typing import Callable
 redis_ = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decortator counter"""
-    @wraps(method)
-    def wrapper(url):
-        """ Wrapper for decorator"""
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@count_requests
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    req = requests.get(url)
-    return req.text
+    """we will implement a get_page function ."""
+    result = requests.get(url).text
+    if not redis_.get("count:{}".format(url)):
+        redis_.set("count:{}".format(url), 1)
+        redis_.setex("result:{}".format(url), 10, result)
+    else:
+        redis_.incr("count:{}".format(url), 1)
+    return result
