a569bd6c9727ecc06c7502a47c38e923dcc7bfe6
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-25 01:09:43 +0300

    Refactor count_requests wrapper function

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,7 +12,7 @@ redis_ = redis.Redis()
 def count_requests(method: Callable) -> Callable:
     """ Decortator counter"""
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
+    def wrapper(url):
         """ Wrapper for decorator"""
         redis_.incr(f"count:{url}")
         cached_html = redis_.get(f"cached:{url}")
