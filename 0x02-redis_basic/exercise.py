c145bd5bead45bae347b1ddba6699bb83ca3bf7b
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-24 14:24:20 +0300

    Add type hinting to store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """Create a Cache class."""
 import redis
 import uuid
+from typing import Union
 
 
 class Cache():
@@ -11,7 +12,7 @@ class Cache():
         self._redis = redis.Redis();
         self._redis.flushdb()
     
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """STORE METHOD"""
         key=str(uuid.uuid4())
         self._redis.set(key, data)
